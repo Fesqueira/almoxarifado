5d97ed509e9edfc6652ca39392073c4ec17aff78
felipebrdev <felipe.rodrigues.siqueira20@gmail.com>
2026-07-17 21:55:35 -0300

    Add services

---
+++ src/main/java/com/almoxarifado/service/ProductService.java
@@ -0,0 +1,13 @@
+
+package almoxarifado.service;
+
+public class ProductService (){
+    private final  List<Product> products;
+
+    public ProductService (){
+        this.products = new ArrayList<>();
+
+    }
+
+
+}
